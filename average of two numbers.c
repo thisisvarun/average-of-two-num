74f2f6d3e26ce46fc22d815447289d394738b83f
thisisvarun <96563987+thisisvarun@users.noreply.github.com>
2022-01-07 11:43:54 +0530

    Add files via upload

---
+++ average of two numbers.c
@@ -0,0 +1,12 @@
+/* A C program to find the average of two numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	int c;
+	printf("enter the values of a,b");
+	scanf("%d%d",&a,&b);
+	printf("the average of the two values is %d");
+	c=a+b/2;
+	return 0;
+}
